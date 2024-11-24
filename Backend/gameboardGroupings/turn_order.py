e1eaec29996a02f63246e252c789b3cbfa09f44b
adbishop <adbisho21@jh.edu>
2024-11-24 13:53:34 -0500

    building the base logic for the GameProcessor. Additional modification and improvements required to integrate with other systems.

---
+++ Backend/gameboardGroupings/turn_order.py
@@ -0,0 +1,27 @@
+from typing import List
+from GameManagement.player import Player
+
+DEFAULT_LIST = []
+
+class TurnOrder():
+    """Manages the order of turnsin the game."""
+    def __init__(self, turn_order: List[Player] = DEFAULT_LIST):
+        self._turn_order: List[Player] = turn_order
+    
+    def add_player(self, player: Player):
+        pass
+
+    def remove_player(self, player_id: int):
+        pass
+
+    def get_current_turn(self):
+        pass
+
+    def advance_turn(self):
+        pass
+
+    def reverse_turn_order(self):
+        pass
+
+    def get_turn_order(self) -> List[Player]:
+        return self._turn_order
