ef244fc962ef01a013e5c21be98d88d96b9d997a
adbishop <adbisho21@jh.edu>
2024-11-26 20:23:14 -0500

    the Django application can successfully compile and run

--- Backend/MessageTranslator/routing.py
+++ Backend/MessageTranslator/routing.py
@@ -3,7 +3,5 @@ from Backend.MessageTranslator import consumers  # Import the consumer
 
 
 websocket_urlpatterns = [
-    re_path(r"ws/notifications/$", consumers.NotificationConsumer.as_asgi()),
-    re_path(r"ws/gameroom/<id>$", consumers.GameRoomConsumer.as_asgi()),
-    re_path(r"ws/gameroom/<id>/chat/$", consumers.GameRoomChatConsumer.as_asgi()),
+    re_path(r"ws/notifications/$", consumers.NotificationConsumer.as_asgi())
 ]
